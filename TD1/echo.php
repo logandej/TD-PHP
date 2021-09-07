48df71d1cf60ca725f8d0c57a2894d6c3a2f9e64
Logan De Jesus <logan.de-jesus@etu.umontpellier.fr>
2021-09-07 15:41:54 +0200

    je suis le boss

--- TD1/echo.php
+++ TD1/echo.php
@@ -42,15 +42,84 @@ END;
 
         echo "<p><strong> 3) Tableaux Associatifs </strong></p>";
 
-        $mon_tableau = array();
 
         $coordonnees['prenom'] = 'Logan';
         $coordonnees['nom'] = 'De Jesus';
+        $coordonnees['date'] = '03/10/02';
+        $coordonnees['age'] = '18';
+        $coordonnees['mail'] = 'logan.dejesus1@gmail.com';
 
         echo $coordonnees['prenom'];
         echo "<p></p>";
         echo $coordonnees['nom'];
 
+        $coordonnees2 = array (
+        'prenom' =>  $coordonnees['prenom'],
+        'nom'    => $coordonnees['nom'] );
+
+        echo $coordonnees2['prenom'];
+        echo $coordonnees2['nom'];
+
+        echo "<p><strong> 4) Foreach </strong></p>";
+
+        foreach ($coordonnees as $cle => $valeur){
+            echo "$cle : $valeur\n";
+            echo "<p></p>";
+        }
+
+        echo "<p><strong> Exo 4 </strong></p>";
+
+        $prenom = "Marc";
+
+        echo "Bonjour " . $prenom;
+        echo "Bonjour $prenom";
+        echo 'Bonjour $prenom';
+
+        echo $prenom;
+        echo "$prenom";
+
+        echo "<p><strong> Exo 5 </strong></p>";
+
+        $marque = 'clio';
+        $immatriculation = 'CS-047-GQ';
+        $couleur = 'orange';
+
+        echo "<p> Voiture $immatriculation de marque $marque (couleur $couleur)";
+
+         $voiture = array (
+            'marque' => 'clio',
+            'couleur' => 'orange',
+            'immatriculation' => 'CS-047-GQ');
+         $voiture1 = array (
+            'marque' => 'renault',
+            'couleur' => 'rouge',
+            'immatriculation' => 'BQ-666-QB');
+         $voiture2 = array (
+            'marque' => 'lambo',
+            'couleur' => 'bleu',
+            'immatriculation' => 'AJ-733-GD');
+         $voiture3 = array (
+            'marque' => 'fiat',
+            'couleur' => 'vert',
+            'immatriculation' => 'JH-343-HD');
+
+
+         echo "<p></p>";
+        echo "<p> Voiture  ",$voiture['immatriculation'] ,"de marque ",$voiture['marque'] ,"(couleur", $voiture['couleur'],")</p>";
+        
+        echo "<p><strong> Listes des voitures: </strong></p>";
+
+        $voitures[] = $voiture;
+        $voitures[] = $voiture1;
+        $voitures[] = $voiture2;
+        $voitures[] = $voiture3;
+
+        //print_r($voitures);
+        echo "<ul>";
+            foreach ($voitures as $cle => $valeur){
+                echo "<li> Voiture  ",$valeur['immatriculation'] ," de marque ",$valeur['marque'] ," (couleur", $valeur['couleur']," )</li>";
+            }
+        echo "</ul>";
 
 
         ?>
