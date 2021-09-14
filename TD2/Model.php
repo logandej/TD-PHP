926a9be528ac2984183ca57e4921ccdfbcb64de6
Logan De Jesus <logan.dejesus1@gmail.com>
2021-09-14 14:57:44 +0200

    je suis le boss

---
+++ TD2/Model.php
@@ -0,0 +1,31 @@
+<?php
+class Model {
+
+  require_once("Conf.php");
+   
+
+  
+
+  private static $pdo = NULL;
+
+  
+
+
+  public static function init(){
+    $hostname=Conf::getHostName();
+    $database_name=Conf::getDataBase();
+    $login=Conf::getLogin();
+    $password=Conf::Password();
+    self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name",$login,$password);
+  }
+   
+   public static function getPDO(){
+    if(self::$pdo==NULL){
+         self::$pdo=is_null();
+    else return self::$pdo;
+    }
+   }
+
+  
+}
+?>
