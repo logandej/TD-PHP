baca6ef3293c29142b5adc46eb743e92ab926fd0
Logan De Jesus <logan.dejesus1@gmail.com>
2021-09-28 14:51:47 +0200

    td4

--- TD3/formGetImmatSQL.php
+++ TD3/formGetImmatSQL.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <div>
@@ -27,13 +28,15 @@
         function getVoitureByImmat($immat) {
             $sql = "SELECT * from voiture2 WHERE immatriculation='$immat'";
             echo "<p>J'effectue la requête \"$sql\"</p>";
-            $rep = Model::$pdo->query($sql);
+            $rep = Model::getPDO()->query($sql);
             $rep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
             return $rep->fetch();
         }
+        //12345678'; TRUNCATE voiture2; --
 
         if (isset($_GET['immatriculation'])) {
             $v = getVoitureByImmat($_GET['immatriculation']);
+            var_dump($v);
             $v-> afficher();
         }
         ?>
