8e421a0c5628b00302e031159cd81773b4ae2cb0
Logan De Jesus <logan.dejesus1@gmail.com>
2021-10-05 13:16:18 +0200

    requete action readAll

--- TD4/controller/ControllerVoiture.php
+++ TD4/controller/ControllerVoiture.php
@@ -1,9 +1,12 @@
 
 <body><link rel="stylesheet" href="../style.css"></body>
 <?php
+require_once ('../model/ModelVoiture.php'); // chargement du modèle
 
-    require_once ('../model/ModelVoiture.php'); // chargement du modèle
-    $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
-    require ('../view/voiture/list.php');  //redirige vers la vue
-
+class ControllerVoiture {
+    public static function readAll() {
+        $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
+        require ('../view/voiture/list.php');  //"redirige" vers la vue
+    }
+}
 ?>
--- TD4/controller/routeur.php
+++ TD4/controller/routeur.php
@@ -2,5 +2,9 @@
 <body><link rel="stylesheet" href="../style.css"></body>
 <?php
 require_once 'ControllerVoiture.php';
-ControllerVoiture::readAll(); // Appel de la méthode statique $action de ControllerVoiture
+// On recupère l'action passée dans l'URL
+$action = $_GET['action'];
+// Appel de la méthode statique $action de ControllerVoiture
+ControllerVoiture::$action();
 ?>
+
