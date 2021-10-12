c5f6657c36d6dc946820cb328d3028cad88dcf74
Logan De Jesus <logan.dejesus1@gmail.com>
2021-10-12 15:56:33 +0200

    TD5

--- TD5/view/voiture/list.php
+++ TD5/view/voiture/list.php
@@ -7,13 +7,17 @@
     </head>
     <body>
     <?php
+    echo htmlspecialchars('<a href="test">Test</a>');
     $tab_v = ModelVoiture::getAllVoitures();
     foreach ($tab_v as $v) {
-        echo '<p> Voiture d\'immatriculation est ' . $v->getImmatriculation() . '.</p>';
+        echo '<p> Voiture d\'immatriculation est  ' . htmlspecialchars($v->getImmatriculation()) . '.</p>';
         echo "<a href = ../controller/routeur.php?action=read&immat={$v->getImmatriculation()}>°details°</a>";
         //https://webinfo.iutmontp.univ-montp2.fr/~dejesusl/PHP/TD4/controller/routeur.php?action=read&immat=CS033GQ
+
     }
+
     ?>
+     <audio controls> <source src="../../uwu1.mp3" type="audio/mpeg" /> </audio>;
     </body>
 </html>
 
